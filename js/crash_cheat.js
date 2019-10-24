c0ec04dac50dc3f414526f92988f0739199f6cc4
aestheticisma <1071191953@qq.com>
2019-10-24 13:27:11 +0800

    Site updated: 2019-10-24 13:27:09

---
+++ js/crash_cheat.js
@@ -0,0 +1,15 @@
+var oldTitle = document.title;
+var titleTime; //標題恢復計時器
+document.addEventListener("visibilitychange", function () {
+  if (document.hidden) {
+    document.querySelector("[rel='icon']").setAttribute("href", "/images/favicon-16x16-next.png");
+    document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
+    clearTimeout(titleTime);
+  } else {
+    document.title = '(ฅ>ω<*ฅ) 噫又好了~';
+    document.querySelector("[rel='icon']").setAttribute("href", "/images/favicon-16x16-next.png");
+    titleTime = setTimeout(function () {
+      document.title = oldTitle;
+    }, 1000);
+  }
+});
